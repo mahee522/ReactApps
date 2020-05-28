c15f06655b677f218566c7a07da3240fc8fc0c6b
Mahender Ambala <mahee522@gmail.com>
2020-05-28 15:15:07 -0500

    intial commit

---
+++ httpcallwithaxios/src/index.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+function Reddit() {
+  const [posts, setPosts] = useState([]);
+
+React.useEffect(() => {
+  axios.get(`https://www.reddit.com/r/reactjs.json`)
+    .then(res => {
+      const newPosts = res.data.data.children
+        .map(obj => obj.data);
+
+      setPosts(newPosts);
+    });
+}, []);
+
+  return (
+    <div>
+      <h1>/r/reactjs</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
+ReactDOM.render(
+  <Reddit />,
+  document.getElementById("root")
+);
